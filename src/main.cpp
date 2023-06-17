77c52c7a816f63ac53c9dbbfc9140a4bc2cc7533
Szymon Lach <szymonlach98@gmail.com>
2023-06-17 15:43:56 +0200

    finalized

--- src/main.cpp
+++ src/main.cpp
@@ -1,173 +1,201 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <SPI.h>
-#include <Adafruit_CCS811.h>
+
+#include "SparkFunCCS811.h"
+
+#include <BH1750.h>
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 // Dane do połączenia z siecią Wi-fi
-const char* ssid = "Wifi dom";
-const char* password = "warszawa";
+const char *ssid = "Wifi dom";
+const char *password = "warszawa";
 
 // MQTT broker login/hasło
-const char* MQTT_username = NULL; 
-const char* MQTT_password = NULL; 
+const char *MQTT_username = NULL;
+const char *MQTT_password = NULL;
 
 // Adres IP server'a MQTT
-const char* mqtt_server = "szymonlach.ddns.net";
+const char *MQTT_SERVER= "szymonlach.ddns.net";
+const uint16_t MQTT_PORT = 9004;
 
-//Kontrola LED
-#define LED_PIN 14 //d1
-//Przycisk
-#define BUTTON_PIN 12 //d2
+// LED
+#define LED_PIN 14 // d5
+
+// Przycisk
+#define BUTTON_PIN 12 // d6
 int lastButtonState;
 
-//CJMCU-811
-Adafruit_CCS811 ccs;
-//CCS811 ccs;
+// DHT22
+#define DHT22_PIN 13 // D7
+#define DHTTYPE DHT22
+DHT_Unified dht(DHT22_PIN, DHTTYPE);
 
+// CJMCU-811
+#define CCS811_ADDR 0x5A
+CCS811 ccs811(CCS811_ADDR);
 
+// BH1750
+#define BH1750_ADDR 0x23
+BH1750 bh1750(BH1750_ADDR);
+
+// Wifi & MQTT & HTTP
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 long now = millis();
 long lastMeasure = 0;
-unsigned long previousMillis = 0; 
+unsigned long previousMillis = 0;
+unsigned long previousSensorMillis = 0;
 
-void setup_wifi() {
-  delay(10);
-  Serial.println();
-  Serial.print("Laczenie z siecia ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Polaczono!, adres IP urzadzenia: ");
-  Serial.println(WiFi.localIP());
+void setupWifi()
+{
+	delay(10);
+	Serial.println();
+	Serial.print("Laczenie z siecia ");
+	Serial.println(ssid);
+	WiFi.begin(ssid, password);
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		delay(500);
+		Serial.print(".");
+	}
+	Serial.println("");
+	Serial.print("Polaczono!, adres IP urzadzenia: ");
+	Serial.println(WiFi.localIP());
 }
 
-void callback(String topic, byte* message, unsigned int length) {
-  Serial.print("Uzyskano odpowiedz na temat ");
-  Serial.print(topic);
-  Serial.print(". Wiadomosc: ");
-  String messageTemp;
-  
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-  }
-  Serial.println();
+void callback(String topic, byte *message, unsigned int length)
+{
+	Serial.print("Uzyskano odpowiedz na temat ");
+	Serial.print(topic);
+	Serial.print(". Wiadomosc: ");
+	String messageTemp;
 
+	for (int i = 0; i < length; i++)
+	{
+		Serial.print((char)message[i]);
+		messageTemp += (char)message[i];
+	}
+	Serial.println();
 
+	if (topic == "led")
+	{
+		if (messageTemp == "on")
+		{
+			digitalWrite(LED_PIN, HIGH);
+		}
+		else if (messageTemp == "off")
+		{
+			digitalWrite(LED_PIN, LOW);
+		}
+	}
 
-  if(topic=="led"){
-      if(messageTemp == "on"){
-        digitalWrite(LED_PIN, HIGH);   
-
-      }
-      else if(messageTemp == "off"){
-        digitalWrite(LED_PIN, LOW);   
-
-      }
-  }
-
-
-  Serial.println();
+	Serial.println();
 }
 
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if (client.connect("ESP8266_2", MQTT_username, MQTT_password)) {
-      Serial.println("connected");  
-      client.subscribe("led");
-     // client.subscribe("przycisk"); //nie subskrybuj bo wysyla wiadomosc
-      client.subscribe("potencjometr");
-      client.subscribe("czujnik_1");
-      client.subscribe("czujnik_2");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
+void reconnect()
+{
+	while (!client.connected())
+	{
+		Serial.print("Proba polaczenia MQTT...");
+		if (client.connect("ESP8266_2", MQTT_username, MQTT_password))
+		{
+			Serial.println("Polaczono!");
+			client.subscribe("led");
+		}
+		else
+		{
+			Serial.print("blad, rc=");
+			Serial.print(client.state());
+			delay(5000);
+		}
+	}
 }
 
-// The setup function sets your ESP GPIOs to Outputs, starts the serial communication at a baud rate of 115200
-// Sets your mqtt broker and sets the callback function
-// The callback function is what receives messages and actually controls the LEDs
-void setup() {
-  Wire.begin(); //I2C
-  Serial.begin(115200);
-  //LED
-  pinMode(LED_PIN, OUTPUT);
-  //PRZYCISK
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  lastButtonState = digitalRead(BUTTON_PIN);
+// Funkcje obsługi czujników
 
-  setup_wifi();
-  client.setServer(mqtt_server, 9004);
-  client.setCallback(callback);
-  //CCS811 | 0x5B
-  if(!ccs.begin(0x5B)){
-    Serial.println("CJMCU-811 nie dziala");
-  }
+void handleDHTSensor()
+{
+	sensors_event_t event;
+	dht.temperature().getEvent(&event);
+	if (!isnan(event.temperature))
+	{
+		client.publish("DHT22/TEMP", String(event.temperature).c_str());
+	}
 
-  while(!ccs.available());
+	dht.humidity().getEvent(&event);
+	if (!isnan(event.relative_humidity))
+	{
+		client.publish("DHT22/HUMIDITY", String(event.relative_humidity).c_str());
+	}
 }
 
-// For this project, you don't need to change anything in the loop function. Basically it ensures that you ESP is connected to your broker
-void loop() {
-  // unsigned long currentMillis = millis();
-    
-
-  // if (!client.connected()) {
-  //   reconnect();
-  // }
-  // if(!client.loop())
-  //   client.connect("ESP8266Client", MQTT_username, MQTT_password);
-
-  // // Publish z interwałem 200ms
-  // if (currentMillis - previousMillis >= 200) {
-  //   previousMillis = currentMillis;
-  //   client.publish("przycisk",String(digitalRead(BUTTON_PIN)).c_str());
-
-
-
-  // } 
-
-
-      
-    if (ccs.available())
-    {
-      if(!ccs.readData()){
-        int tempCO2 = ccs.geteCO2();
-        int tempVOC = ccs.getTVOC();
-        client.publish("CCS811/CO2",String(tempCO2).c_str());
-        client.publish("CCS811/TVOC",String(tempVOC).c_str());
-        Serial.print("ppm, TVOC: ");
-        Serial.println(tempVOC);
-        Serial.print("ppb Temp:");
-        Serial.println(tempCO2);
-      }else{
-        Serial.println("ERROR!");
-      }
-
-    }
-    delay(500);
-    // if(ccs.available()){
-    //     float temp = ccs.calculateTemperature();
-    //     if(!ccs.readData()){
-    //     Serial.print("CO2: ");
-    //     Serial.print(ccs.geteCO2());
-    //     Serial.print("ppm, TVOC: ");
-    //     Serial.print(ccs.getTVOC());
-    //     Serial.print("ppb Temp:");
-    //     Serial.println(temp);
-    //     }
-    // }
+void handleBH1750Sensor()
+{
+	float lux = bh1750.readLightLevel();
+	client.publish("BHT1750", String(lux).c_str());
 }
 
+void handleCCS811Sensor()
+{
+	if (ccs811.dataAvailable())
+	{
+		ccs811.readAlgorithmResults();
+		int tempCO2 = ccs811.getCO2();
+		int tempVOC = ccs811.getTVOC();
+		client.publish("CCS811/CO2", String(tempCO2).c_str());
+		client.publish("CCS811/TVOC", String(tempVOC).c_str());
+	}
+}
+
+void setup()
+{
+	Wire.begin(); // I2C
+	Serial.begin(115200);
+	// LED
+	pinMode(LED_PIN, OUTPUT);
+	// PRZYCISK
+	pinMode(BUTTON_PIN, INPUT_PULLUP);
+	lastButtonState = digitalRead(BUTTON_PIN);
+
+	//Wifi & MQTT
+	setupWifi();
+	client.setServer(MQTT_SERVER, MQTT_PORT);
+	client.setCallback(callback);
+
+	dht.begin();
+	ccs811.begin();
+	bh1750.begin();
+}
+
+void loop()
+{
+	if (!client.connected())
+	{
+		reconnect();
+	}
+	if (!client.loop())
+	{
+		client.connect("ESP8266Client", MQTT_username, MQTT_password);
+	}
+
+	// "Asynchronicznie" żeby nie przeciążać serwera MQTT ilością danych.
+	unsigned long currentMillis = millis();
+	if (currentMillis - previousMillis >= 100)
+	{
+		previousMillis = currentMillis;
+		client.publish("przycisk", String(digitalRead(BUTTON_PIN)).c_str());
+	}
+
+	if (currentMillis - previousSensorMillis >= 1000)
+	{
+		previousSensorMillis = currentMillis;
+		handleDHTSensor();
+		handleBH1750Sensor();
+		handleCCS811Sensor();
+	}
+}
